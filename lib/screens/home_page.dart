2b8461da0b46b5d18b58643e44f5f6285d416314
Nestor <nestor.ocamposanchez01@utrgv.edu>
2021-01-22 19:49:50 -0600

    First implementation of the splash screen.
    Implemented splash screen. I had to change the homepage for this to work. I also created a screens folder with individual screens and moved the existin main screen to the home_page screen.

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('My Page!')),
+      drawer: Container(
+        width: 200,
+
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('More'),
+                decoration: BoxDecoration(
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+              ListTile(
+                title: Text('Events'),
+                // trailing: Icon(Icons.more_vert) ,
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Projects'),
+                onTap: () {
+                  //final snackBar = SnackBar(content: Text("Mission statement"));
+
+                  // Scaffold.of(context).showSnackBar(snackBar);
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('About'),
+                trailing: Icon(Icons.more_vert),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Contact Us'),
+                trailing: IconButton(
+                  icon: Icon(Icons.more_vert),
+                  color: Colors.grey,
+                ),
+                onTap: () {
+                  child:
+                  ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text('Email'),
+                        dense: true,
+                      ),
+                    ),
+                  );
+                  // Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Application'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:UTRGV_App/screens/home_page.dart';
+
+final appTitle = 'Drawer Demo';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 3);
+    return new Timer(duration, navigateToDeviceScreen);
+  }
+
+  navigateToDeviceScreen() {
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => MyHomePage(title: appTitle)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xFFF05023), Colors.blueAccent[700]])),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 80,
+              ),
+              Image.asset(
+                'assets/images/ieee1.png',
+                alignment: Alignment.topCenter,
+                height: 200,
+                width: 300,
+                fit: BoxFit.fitWidth,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Center(
+                child: Text(
+                  "Advancing Technology for Humanity",
+                  style: TextStyle(
+                      color: Color(0xFF00629B),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
